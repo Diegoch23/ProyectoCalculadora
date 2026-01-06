235edcf311ff67f6c472e817b9e3e865579cfc96
Diegoch23 <diegochancusisimbana@gmail.com>
2026-01-06 09:24:36 -0500

    PROYECTO

---
+++ calculadoraApp.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using calculadoraApp; 
+
+namespace calculadoraApp.Tests
+{
+    public class CalculadoraTest
+    {
+        [Fact]
+        public void Sumar_DosNumeros_VerificaSumaCorrecta()
+        {
+            var calc = new Calculadora();
+
+            var resultado = calc.Sumar(2, 3);
+
+            Assert.Equal(5, resultado);
+        }
+    }
+}
---
+++ calculadoraApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace calculadoraApp 
+{
+    public class Calculadora
+    {
+        public int Sumar(int valor1, int valor2) => valor1 + valor2;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cal = new Calculadora();
+            Console.WriteLine($"2 + 3 = {cal.Sumar(2, 3)}");
+        }
+    }
+}
